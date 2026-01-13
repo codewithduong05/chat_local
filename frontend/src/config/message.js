49cb8eb9e715aa2749f2b76b5bec6bb49dc90a2d
codewithduong05 <dbui0025@gmail.com>
2026-01-13 19:40:33 +0700

    push message

---
+++ frontend/src/config/message.js
@@ -0,0 +1,15 @@
+const messageHttp = {
+    success : {
+        login : "Login successfull",
+        getAllChatRoomId : "Get all chat room id successfull",
+        createChat : "Create chat successfull",
+        getAllChatRoomId : "Get all chat successfull"
+    },
+    error : {
+        401 : "Unauthorized",
+        404 : "Not found data",
+        500 : "Server is error"
+    }
+}
+
+export default messageHttp
