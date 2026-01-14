cae849c1a73bd172c3bd7fba958189578478a6c7
codewithduong05 <dbui0025@gmail.com>
2026-01-14 18:40:46 +0700

    fix error handlder

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,17 +4,31 @@ import dotenv from "dotenv"
 dotenv.config()
 import connectDB from "./config/monggo.js";
 import RootRouter from "./routes/routes.js";
+import notFoundHandler from "./middlewares/notFoundHandler.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 
 
 
 const app = express();
+
+app.use(cors());
 app.use(express.json())
 
+//  test enpoint
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hello" });
+});
 // endpoints : http://{url}/api/v1
 app.use(RootRouter)
 
-app.listen(3000, () => {
-    console.log("Server running ...");
+app.use(notFoundHandler);
+app.use(errorHandler);
+const logUrl = process.env.LOG_URL_BACKEND || "http://localhost:";
+app.listen(process.env.PORT_BACKEND, () => {
+    console.log("Server is running " );
+    console.log("url:" +logUrl + process.env.PORT_BACKEND);
+    console.log("---");
+    
     connectDB()
 })
