ab0d03b600ff341c3a4a2bbf08069750ed8b5625
codewithduong05 <dbui0025@gmail.com>
2026-01-16 15:08:36 +0700

    feat : function get devices model

---
+++ frontend/src/utils/devices.js
@@ -0,0 +1,14 @@
+export const isMobileDevice = () => {
+  const toMatch = [
+    /Android/i,
+    /webOS/i,
+    /iPhone/i,
+    /iPad/i,
+    /iPod/i,
+    /BlackBerry/i,
+    /Windows Phone/i,
+  ];
+  return toMatch.some((toMatchItem) => navigator.userAgent.match(toMatchItem));
+}
+
+export const isDesktopDevice = () => !isMobileDevice();
