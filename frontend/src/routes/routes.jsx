7e8bcb9de9a675e8c908fb20d2bb7cb4a74f292d
codewithduong05 <dbui0025@gmail.com>
2026-01-17 16:30:44 +0700

    create route chatadmin ui

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -2,10 +2,12 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import Login from "@/pages/Login";
 import ResponsiveLayout from "@/layouts/ResponsiveLayout";
 import ChatWindowUI from "@/features/chat/ChatWindow";
+import ChatAdmin from "@/pages/ChatAdmin";
 
 const router = createBrowserRouter([
   { path: "/", element: <div>Hell World</div> },
   { path: "/chat/login", element: <Login /> },
+  {path:"/chat/admin", element: <ChatAdmin/>},
   {
     path: "/chat", element:
       <ResponsiveLayout>
